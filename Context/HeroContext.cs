88e418daeb736819bcf0ccf6796316071cca98dd
jules <jules.wagheamcker99@gmail.com>
2023-05-04 10:28:50 +0200

    Ajoutez des fichiers projet.

---
+++ Context/HeroContext.cs
@@ -0,0 +1,13 @@
+﻿using APIHERO.Model;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace APIHERO.Context
+{
+    public class HeroContext : DbContext
+    {
+
+        public DbSet<Hero> Heroes { get; set; }
+
+    }
+}
---
+++ Model/Hero.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace APIHERO.Model
+{
+   
+    public class Hero
+    {
+        public DbSet<Hero> Heroes { get; set; }
+        public string Name { get; set; }
+        public string Power { get; set; }
+        
+
+    }
+}
